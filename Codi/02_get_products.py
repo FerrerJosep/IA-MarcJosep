079d2213f29320c3df01db763cae45a7b0a18e05
Marc Casa <marmarsan41@alu.edu.gva.es>
2025-02-23 18:40:17 +0100

    Cambios en el scraping y main.ipynb eliminado

---
+++ Codi/02_get_products.py
@@ -0,0 +1,43 @@
+import requests
+import concurrent.futures
+import json
+
+API_KEYS = [
+    "000f8774b5f8782161820d3a038fddc4"
+]
+current_api_index = 0 
+products_data = []
+
+with open("./Data/asins.txt", "r") as file:
+    ASIN_LIST = [line.strip() for line in file.readlines()]
+
+def fetch_product_data(asin):
+    global current_api_index
+    for _ in range(len(API_KEYS)):
+        payload = {
+            'api_key': API_KEYS[current_api_index],
+            'url': f'https://www.amazon.es/dp/{asin}',
+            'output_format': 'json',
+            'autoparse': 'true',
+            'retry_404': 'true'
+        }
+        response = requests.get('https://api.scraperapi.com/', params=payload) 
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error con API_KEY {current_api_index + 1}: {API_KEYS[current_api_index]}")
+            current_api_index = (current_api_index + 1) % len(API_KEYS)
+            print(f"Usando API_KEY {current_api_index + 1}: {API_KEYS[current_api_index]}")
+    return None
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    results = executor.map(fetch_product_data, ASIN_LIST)
+
+for product_info in results:
+    if product_info is not None:
+        products_data.append(product_info)
+        
+with open("./Data/products.json", "w", encoding="utf-8") as f:
+    json.dump(products_data, f, ensure_ascii=False, indent=4)
+
+print("Datos guardados en products.json")
---
+++ Codi/03_build_dataset.py
@@ -0,0 +1,54 @@
+import csv
+import json
+import re
+
+def find_value_by_keywords(data_dict, keywords, pattern=None, default="null"):
+    for key, value in data_dict.items():
+        if any(keyword.lower() in key.lower() for keyword in keywords):
+            if pattern is None or re.search(pattern, str(value)):
+                return value
+    return default
+
+def safe_get(value, default="null"):
+    return str(value).replace("€", "").strip() if value else default
+
+with open('./Data/products.json', 'r', encoding='utf-8') as json_file:
+    products_data = json.load(json_file)
+
+with open('moviles.csv', mode='w', newline='', encoding='utf-8-sig') as file:
+    writer = csv.writer(file)
+    writer.writerow(["URL", "Asin", "Precio", "Precio Inicial", "Título", "Estrellas", "Opiniones", "Marca", "Modelo", "Año del modelo", "Dimensiones",
+                     "RAM", "Memoria", "Sistema operativo", "Resolución pantalla", "Tamaño pantalla", "Relación aspecto", "Peso", "Tecnología conectividad", "Batería"])
+
+    for product in products_data:
+        product_information = product.get("product_information", {})
+        
+        url = safe_get(product.get("brand_url"))
+        asin = safe_get(product_information.get("ASIN"))
+        product_price = safe_get(product.get("pricing"))
+        product_original_price = safe_get(product.get("list_price", product_price))
+        
+        product_title = safe_get(product.get("name"))
+        product_star_rating = safe_get(product.get("average_rating"))
+        product_num_ratings = safe_get(product.get("total_reviews"))
+
+        marca = find_value_by_keywords(product_information, ["fabricante", "marca"])
+        modelo = find_value_by_keywords(product_information, ["modelo"])
+        ano = find_value_by_keywords(product_information, ["año"], r"\b\d{4}\b")
+        dimensiones = find_value_by_keywords(product_information, ["dimensiones", "tamaño", "medidas"], r"\d+")
+        ram = find_value_by_keywords(product_information, ["RAM", "memoria RAM"], r"\b(1|2|4|8|16|32|64)\s?GB\b")
+        memoria = find_value_by_keywords(product_information, ["ROM", "memoria", "capacidad"], r"\b(32|64|128|256|512|1024)\s?(GB|TB)\b")
+        sistema_operativo = safe_get(product_information.get("Sistema operativo"))
+        resolucion_pantalla = find_value_by_keywords(product_information, ["resolucion", "resolución"], r"\d+")
+        tamano_pantalla = find_value_by_keywords(product_information, ["pantalla", "pulgadas"], r"\d+")
+        relacion_aspecto = find_value_by_keywords(product_information, ["relacion", "aspecto"], r"\d+:\d+")
+        peso = find_value_by_keywords(product_information, ["peso", "masa"], r"\b\d+\s?g\b")
+        tecnologia_conectividad = find_value_by_keywords(product_information, ["tecnología", "conectividad"], r"\b(2G|3G|4G|5G)\b")
+        bateria = find_value_by_keywords(product_information, ["batería", "capacidad batería"], r"\b\d+\s?mAh\b")
+
+        writer.writerow([
+            url, asin, product_price, product_original_price, product_title, product_star_rating, product_num_ratings,
+            marca, modelo, ano, dimensiones, ram, memoria, sistema_operativo, resolucion_pantalla, tamano_pantalla, relacion_aspecto, peso, tecnologia_conectividad, bateria
+        ])
+
+print("Datos guardados en moviles.csv")
