527139992235df8cb65aa463dfdf83027ad82aae
Marc <116910394+MarcMS11@users.noreply.github.com>
2025-02-24 16:59:45 +0100

    Primera ejecución de asins.txt to products.jsonl

--- Codi/02_get_products.py
+++ Codi/02_get_products.py
@@ -1,17 +1,24 @@
 import requests
 import concurrent.futures
 import json
+import os
 
 API_KEYS = [
-    "000f8774b5f8782161820d3a038fddc4"
+    "000f8774b5f8782161820d3a038fddc4",
+    "e8b3c716e689e349595622c51b62f3a0"
 ]
 current_api_index = 0
-products_data = []
 processed_count = 0
+
 with open("./Data/asins.txt", "r") as file:
     ASIN_LIST = [line.strip() for line in file.readlines()]
+
 total_asins = len(ASIN_LIST)
 progress_interval = max(1, total_asins // 100)
+output_file = "./Data/products.jsonl"
+
+if not os.path.exists(output_file):
+    open(output_file, "w").close()
 
 def fetch_product_data(asin):
     global current_api_index
@@ -23,7 +30,7 @@ def fetch_product_data(asin):
             'autoparse': 'true',
             'retry_404': 'true'
         }
-        response = requests.get('https://api.scraperapi.com/', params=payload) 
+        response = requests.get('https://api.scraperapi.com/', params=payload)
         if response.status_code == 200:
             return response.json()
         else:
@@ -32,17 +39,19 @@ def fetch_product_data(asin):
             print(f"Usando API_KEY {current_api_index + 1}: {API_KEYS[current_api_index]}")
     return None
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+def save_to_file(data):
+    with open(output_file, "a", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False)
+        f.write("\n")
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
     results = executor.map(fetch_product_data, ASIN_LIST)
     for product_info in results:
         if product_info is not None:
-            products_data.append(product_info)
+            save_to_file(product_info)
         processed_count += 1
         if processed_count % progress_interval == 0 or processed_count == total_asins:
             progress = (processed_count / total_asins) * 100
             print(f"Progreso: {processed_count}/{total_asins} ({progress:.2f}%)")
 
-with open("./Data/products.json", "w", encoding="utf-8") as f:
-    json.dump(products_data, f, ensure_ascii=False, indent=4)
-
-print("Datos guardados en products.json")
+print("Proceso completado. Datos guardados en products.jsonl")
