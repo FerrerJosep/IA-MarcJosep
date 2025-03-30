968462ae27938507d873319208bb220073f303bf
Marc Casa <marmarsan41@alu.edu.gva.es>
2025-03-30 18:47:20 +0200

    Cambiadas rutas

--- Projecte2/datasets/josep/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Matriculas Españolas > 2022-11-15 8:33pm
-https://universe.roboflow.com/vc/matriculas-espanolas
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- Projecte2/datasets/josep/model.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Cargar YOLO
-model = YOLO("yolov8n.pt")
-
-# Entrenar con el archivo data.yaml corregido
-model.train(data="C:/Users/Josep/Desktop/PARKING/datasets/data.yaml", epochs=50, imgsz=640)
