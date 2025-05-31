fc79dd3c04d58f61fd2d1086e71f3fc68e660ee8
FerrerJosep <joseepferreer@gmail.com>
2025-05-31 18:53:41 +0200

    proves...

--- Projecte3/interfaz.py
+++ Projecte3/interfaz.py
@@ -1,1 +1,13 @@
-print("Prova")
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Interfaz de Flet"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    page.add(ft.Text("Hola, Flet!"))
+    page.add(ft.ElevatedButton("Click me!", on_click=lambda _: print("Button clicked!")))
+
+
+if __name__ == "__main__":
+    ft.app(target=main)
